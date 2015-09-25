9bc4a6bed205373b4bd33ff92b5755370f63ef68
shy1 <sethyoungzip@gmail.com>
2015-09-25 16:50:39 -0400

    player class branch initial commit

---
+++ src/main/java/io/illcoder/casinoRoyale/core/Player.java
@@ -0,0 +1,54 @@
+package io.illcoder.casinoRoyale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sstrauss on 9/24/15.
+ */
+public class Player {
+    private String name;
+    private int money = 500;
+    private List<Card> hand = new ArrayList<Card>();
+
+
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public Player(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void addCard(Card card) {
+        this.hand.add(card);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+
+    public Card getHandCard(int _i){
+        return hand.get(_i);
+    }
+
+}
+
