b021d8eb63ce721c38a44dc9fdbd652d144361f8
shy1 <sethyoungzip@gmail.com>
2015-09-25 21:04:46 -0400

    first dealer changes

--- src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
@@ -3,54 +3,47 @@ package io.illcoder.casinoRoyale.core;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Created by syoung on 9/22/15.
  */
 public class Dealer {
-    private int wager;
 
-    public String compareHands(){
-        return "Player wins";
-    }
-
-    /**
-     * Dealer constructor.
-     */
+    private Deck deck = new Deck;
+    private List<Card> cardDeck = deck.getShuffledDeck();
+    private int cardIndex = 0;
     public Dealer(){
 
     }
 
 
-    public Deck createDeck(){
-        return new Deck();
-    }
-
-
 
     /**
-     * Starts the game and
-     * @return Message to user letting them know which game has started.
-     */
-    public void runGame() throws IOException {
-        String gameMessage= "Starting game... ";
-        Boolean continueLoop = true;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        while(continueLoop){
-
-
-            System.out.println("Enter response");
-            String input = br.readLine();
-            if (input.equals("quit"))  continueLoop = false;
-
-
-
-
-
+     * Starts a game depending on the gameType argument that is passed.
+     *
+     *
+    public void startGame(int _gametype, Player _user) {
+        switch (_gametype) {
+            case 1:
+                BlackjackEngine game = new BlackjackEngine;
+                game.runGame(_user);
+                break;
+            case 2:
+                PokerEngine game = new PokerEngine;
+                game.runGame(_user);
+                break;
         }
-
+     }
+*/
+    public Card dealCard() {
+        Card topCard = cardDeck[cardIndex];
+        cardIndex++;
+        return topCard;
     }
+
+
+
 
     /**
      * Deals hand to the player and CPU and
