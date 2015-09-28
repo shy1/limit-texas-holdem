547ac9497fcb8aba0bc22d2735a98d58705230f7
Seth Young <sethyoung1@gmx.com>
2015-09-28 15:25:54 -0400

    added cardIndex reset to dealer.shuffleDeck

--- src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
@@ -21,19 +21,19 @@ public class Dealer {
      * Starts a game depending on the gameType argument that is passed.
      *
      *
-    public void startGame(int _gametype, Player _user) {
-        switch (_gametype) {
-            case 1:
-                BlackjackEngine game = new BlackjackEngine;
-                game.runGame(_user);
-                break;
-            case 2:
-                PokerEngine game = new PokerEngine;
-                game.runGame(_user);
-                break;
-        }
+     public void startGame(int _gametype, Player _user) {
+     switch (_gametype) {
+     case 1:
+     BlackjackEngine game = new BlackjackEngine;
+     game.runGame(_user);
+     break;
+     case 2:
+     PokerEngine game = new PokerEngine;
+     game.runGame(_user);
+     break;
      }
-*/
+     }
+     */
 
     /**
      * Deals a single card from the deck array and increments
@@ -48,9 +48,15 @@ public class Dealer {
 
     /**
      * Shuffles the cardDeck arraylist by calling the deck class's shuffleDeck function
+     * and resets the cardIndex counter.
      */
     public void shuffleDeck() {
         deck.shuffleDeck(cardDeck);
+        cardIndex = 0;
+    }
+
+    public int getCardIndex(){
+        return cardIndex;
     }
 
     /**
